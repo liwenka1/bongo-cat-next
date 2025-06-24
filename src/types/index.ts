a5c9b6e300acaa7d5355090c913d52c0f7bb17c1
unknown <2020583117@qq.com>
2025-06-24 23:15:40 +0800

    clean: 清理未使用的组件类型定义

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,8 +7,7 @@ export type { AppWindowState, HookWindowState } from "./window";
 // Live2D相关类型
 export type { Live2DModel, Live2DApp, Live2DInstance, ModelJSON } from "./live2d";
 
-// 组件相关类型
-export type { LogoProps, RoundedButtonProps } from "./components";
+
 
 // 监视器相关类型
 export type { CursorPosition, Monitor } from "./monitor";
